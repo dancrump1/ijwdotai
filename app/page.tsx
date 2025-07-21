7647ec8c86988117bb7cfee2fde890a60fb9c2a4
dancrump1 <danielcrump@outlook.com>
2025-07-21 13:30:22 -0400

    use public folder to get files to client

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,17 @@ import path from "path";
 import Home from "./homepage";
 
 function getComponentFiles(): string[] {
-	const dirPath = path.join(process.cwd(), "public", "my-files");
+	const dirPath = path.join(process.cwd(), "public", "r");
 
 	try {
-		return fs.readdirSync(dirPath);
+		return fs
+			.readdirSync(dirPath)
+			.filter((file) => fs.statSync(path.join(dirPath, file)).isFile()); // Only return file names
 	} catch (err) {
-		console.error("Error reading my-files directory:", err);
+		console.error("Error reading directory:", err);
 		return [];
 	}
 }
-
 export default async function Page() {
-	const componentFiles = "";
-
 	return <Home files={getComponentFiles()} />;
 }

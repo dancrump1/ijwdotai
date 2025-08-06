c2a639ff2c9d0b25346441fd85bdfd15dc7973a1
dancrump1 <danielcrump@outlook.com>
2025-08-06 09:03:24 -0400

    Revert "remove drivedev and isnew"
    This reverts commit 0e1e9fde268d665c82e668918f9d9bb112b699b9.

--- components/PromptComponent.tsx
+++ components/PromptComponent.tsx
@@ -157,7 +157,11 @@ export default function PromptComponent({
 	const [isPromptExpanded, setIsPromptExpanded] = useState(initialExpanded);
 	const [shouldAnimate, setShouldAnimate] = useState(false);
 	const [isDialogOpen, setIsDialogOpen] = useState(false);
-
+	const initialAttachments = registrarComponents.map((item) => {
+		return {
+			url: `https://components.drivedev.net/r/${item}.json`,
+		};
+	});
 	const [attachments, setAttachments] = useState<Attachment[]>([]);
 	const [selectedComponents, setSelectedComponents] = useState([]);
 	const [isListening, setIsListening] = useState(false);
@@ -209,7 +213,7 @@ export default function PromptComponent({
 	useEffect(() => {
 		setAttachments(
 			selectedComponents.map((item) => ({
-				url: `https://ijwdotai.com/r/${item}`,
+				url: `https://components.drivedev.net/r/${item}.json`,
 			}))
 		);
 	}, [selectedComponents]);
--- registry/open-source/3d-nav-bar.tsx
+++ registry/open-source/3d-nav-bar.tsx
@@ -307,7 +307,7 @@ const NavBar = ({ routes }: { routes: any[] }) => {
 						})}
 					<Link
 						prefetch={false}
-						href={"https://www.admin.example.com/access"}
+						href={"https://www.admin.example.drivedev.net/access"}
 						key={"admin"}
 						onMouseEnter={() => {
 							setHovered(null);

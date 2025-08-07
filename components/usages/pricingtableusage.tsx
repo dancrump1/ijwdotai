85abaaf3b219893c83e594993c06b797183815c0
dancrump1 <danielcrump@outlook.com>
2025-08-07 10:04:55 -0400

    fix pricing table

--- components/usages/pricingtableusage.tsx
+++ components/usages/pricingtableusage.tsx
@@ -2,123 +2,163 @@
 
 import React from "react";
 
-import Image from "next/image";
-
-import {
-	ProgressCarousel,
-	SliderBtn,
-	SliderBtnGroup,
-	SliderContent,
-	SliderWrapper,
-} from "@/registry/open-source/progress-carousel";
-import { Home } from "lucide-react";
-import { useMediaQuery } from "usehooks-ts";
+import { CheckIcon } from "lucide-react";
 
 export default function Usage() {
-	const isMobile = useMediaQuery("(min-width: 640px)");
-
 	return (
 		<div className="h-screen w-full flex items-center justify-center relative overflow-hidden bg-black">
-			<ProgressCarousel
-				vertical={isMobile ? true : false}
-				fastDuration={300}
-				duration={4000}
-				activeSlider="bbridge"
-				className="sm:flex "
-			>
-				<SliderBtnGroup className="sm:relative absolute bottom-0 lg:w-[28rem] sm:w-96 w-full z-10 sm:flex sm:flex-col grid grid-cols-2 sm:h-[500px] h-fit sm:dark:bg-black sm:bg-white dark:bg-black/80 bg-white/80 backdrop-blur-md overflow-hidden ">
-					{[
-						{
-							icon: <Home />,
-							img: "/itjustworks.jpg",
-							title: "Bridge",
-							desc: "A breathtaking view of a city illuminated by countless lights, showcasing the vibrant and bustling nightlife.",
-							sliderName: "bbridge",
-						},
-						{
-							icon: <Home />,
-							img: "/itjustworks.jpg",
-							title: "Mountains View",
-							desc: "A serene lake reflecting the surrounding mountains and trees, creating a mirror-like surface.",
-							sliderName: "bmountains",
-						},
-						{
-							icon: <Home />,
-							img: "/itjustworks.jpg",
-							title: "Autumn",
-							desc: "A picturesque path winding through a dense forest adorned with vibrant autumn foliage.",
-							sliderName: "bautumn",
-						},
-						{
-							icon: <Home />,
-							img: "/itjustworks.jpg",
-							title: "Foggy",
-							sliderName: "bfoggy",
-							desc: "A stunning foggy view over the foresh, with the sun casting a golden glow across the forest. ",
-						},
-					].map((item, index) => (
-						<SliderBtn
-							key={item?.sliderName + "button"}
-							value={item?.sliderName + "button"}
-							className="text-left  p-3 sm:border-b border sm:pl-5 sm:pb-0 pb-6 sm:flex-1"
-							progressBarClass="left-0 sm:top-0 bottom-0 dark:bg-white bg-black sm:w-3 sm:h-full h-4  before:h-full before:w-4 before:"
-						>
-							<span className="relative px-4 rounded w-fit dark:bg-blue-500 bg-black text-white mb-2">
-								{item.title}
-							</span>
-							<span className="text-sm font-medium dark:text-slate-200 text-slate-900 line-clamp-2">
-								{item.desc}
-							</span>
-						</SliderBtn>
-					))}
-				</SliderBtnGroup>
-				<SliderContent className="w-full">
-					{[
-						{
-							icon: <Home />,
-							img: "/itjustworks.jpg",
-							title: "Bridge",
-							desc: "A breathtaking view of a city illuminated by countless lights, showcasing the vibrant and bustling nightlife.",
-							sliderName: "bbridge",
-						},
-						{
-							icon: <Home />,
-							img: "/itjustworks.jpg",
-							title: "Mountains View",
-							desc: "A serene lake reflecting the surrounding mountains and trees, creating a mirror-like surface.",
-							sliderName: "bmountains",
-						},
-						{
-							icon: <Home />,
-							img: "/itjustworks.jpg",
-							title: "Autumn",
-							desc: "A picturesque path winding through a dense forest adorned with vibrant autumn foliage.",
-							sliderName: "bautumn",
-						},
-						{
-							icon: <Home />,
-							img: "/itjustworks.jpg",
-							title: "Foggy",
-							sliderName: "bfoggy",
-							desc: "A stunning foggy view over the foresh, with the sun casting a golden glow across the forest. ",
-						},
-					].map((item, index) => (
-						<SliderWrapper
-							className="h-full"
-							key={item.sliderName + "wrapper"}
-							value={item?.sliderName + "wrapper"}
-						>
-							<Image
-								className=" h-[500px] object-cover"
-								src={item.img}
-								width={1900}
-								height={1080}
-								alt={item.desc}
-							/>
-						</SliderWrapper>
-					))}
-				</SliderContent>
-			</ProgressCarousel>{" "}
+			<section className="py-24 lg:pb-32  overflow-hidden text-neutral-800 dark:text-neutral-50">
+				<div className="container px-4 mx-auto">
+					<div className="max-w-2xl mx-auto text-center mb-20">
+						<h2 className="mb-4 text-6xl tracking-tighter">
+							Pricing &amp; Plans
+						</h2>
+						<p className="text-xl tracking-tight">
+							Use and reuse tons of responsive sections to create the
+							perfect layout. Sections are ready.
+						</p>
+					</div>
+					<div className="flex flex-wrap -m-6 *:mx-auto">
+						<div className="w-full md:w-1/2 lg:w-1/3 p-6">
+							<div className="h-full bg-white dark:bg-neutral-900 border border-neutral-300 dark:border-neutral-600 rounded-2xl transform-gpu hover:-translate-y-2 transition duration-500 ">
+								<div className="p-12 border-b border-neutral-300 dark:border-neutral-600">
+									<div className="pr-9">
+										<h4 className="mb-6 text-6xl tracking-tighter">
+											Solo
+										</h4>
+										<p className="mb-2 text-xl font-semibold tracking-tight">
+											From $29/mo
+										</p>
+										<p className="tracking-tight">
+											The ideal plan for larger businesses who
+											require heavy usage.
+										</p>
+									</div>
+								</div>
+								<div className="p-12 pb-11">
+									<ul className="-m-1.5 mb-11">
+										<FeatureItem>50 Users</FeatureItem>
+										<FeatureItem>Unlimited Projects</FeatureItem>
+										<FeatureItem>Project Schedule</FeatureItem>
+										<FeatureItem>150+ Integrations</FeatureItem>
+										<FeatureItem>Priority Email Support</FeatureItem>
+									</ul>
+									<PricingButton noCardRequired={true}>
+										Try 14 Days Free Trial
+									</PricingButton>
+								</div>
+							</div>
+						</div>
+						<div className="w-full md:w-1/2 lg:w-1/3 p-6">
+							<div
+								className="p-px overflow-hidden rounded-2xl hover:-translate-y-2 transition duration-500 transform-gpu"
+								style={{
+									backgroundImage:
+										"url('/background-image/advanced-gradient.jpg')",
+									backgroundRepeat: "no-repeat",
+									backgroundSize: "cover",
+								}}
+							>
+								<div className="h-full bg-white dark:bg-neutral-900 rounded-2xl">
+									<div
+										className="p-12"
+										style={{
+											backgroundImage:
+												"url('/background-image/advanced-gradient.jpg')",
+											backgroundRepeat: "no-repeat",
+											backgroundSize: "cover",
+										}}
+									>
+										<div className="pr-9">
+											<h4 className="mb-6 text-6xl text-white tracking-tighter">
+												Startup
+											</h4>
+											<p className="mb-2 text-xl text-white font-semibold tracking-tighter">
+												From $99/mo
+											</p>
+											<p className="text-white tracking-tight">
+												The ideal plan for larger businesses who
+												require heavy usage.
+											</p>
+										</div>
+									</div>
+									<div className="p-12 pb-11">
+										<ul className="-m-1.5 mb-11">
+											<FeatureItem>50 Users</FeatureItem>
+											<FeatureItem>Unlimited Projects</FeatureItem>
+											<FeatureItem>Project Schedule</FeatureItem>
+											<FeatureItem>150+ Integrations</FeatureItem>
+											<FeatureItem>
+												Priority Email Support
+											</FeatureItem>
+										</ul>
+										<PricingButton noCardRequired={true}>
+											Try 14 Days Free Trial
+										</PricingButton>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="w-full md:w-1/2 lg:w-1/3 p-6">
+							<div className="flex flex-col justify-between h-full bg-white dark:bg-neutral-900 border border-neutral-300 dark:border-neutral-600 rounded-2xl transform-gpu hover:-translate-y-2 transition duration-500">
+								<div className="p-12 border-neutral-300 dark:border-neutral-600">
+									<div className="pr-9">
+										<h4 className="mb-6 text-6xl tracking-tighter">
+											Custom
+										</h4>
+										<p className="mb-2 text-xl font-semibold tracking-tighter">
+											From $399/mo
+										</p>
+										<p className="tracking-tight">
+											We can customize a plan that suits the exact
+											needs of your business.
+										</p>
+									</div>
+								</div>
+								<div className="p-12 pb-11">
+									<PricingButton>Contact sales</PricingButton>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
 		</div>
 	);
 }
+
+const FeatureItem = ({ children }: { children: string }) => {
+	return (
+		<li className="flex items-center py-1.5">
+			<CheckIcon className="size-3 mr-3" />
+			<span className="font-medium tracking-tight">{children}</span>
+		</li>
+	);
+};
+
+const PricingButton = ({
+	children,
+	href,
+
+	noCardRequired,
+}: {
+	children: string;
+	href?: string;
+	noCardRequired?: boolean;
+}) => {
+	return (
+		<>
+			<a
+				className="inline-block px-5 py-4 w-full text-center  font-semibold tracking-tight bg-transparent hover:bg-neutral-900 hover:text-white border dark:hover:bg-white dark:hover:text-neutral-800 hover:scale-105 border-neutral-700 rounded-lg transition duration-200"
+				href={href ?? ""}
+			>
+				{children}
+			</a>
+			{noCardRequired && (
+				<span className="text-sm text-neutral-600 tracking-tight">
+					No credit card required
+				</span>
+			)}
+		</>
+	);
+};

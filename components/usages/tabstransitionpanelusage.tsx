6b39775b03565471527cbd03af4cbd093ad5cd08
dancrump1 <danielcrump@outlook.com>
2025-08-14 13:47:47 -0400

    use the correct component

--- components/usages/tabstransitionpanelusage.tsx
+++ components/usages/tabstransitionpanelusage.tsx
@@ -2,12 +2,12 @@
 
 import React from "react";
 
-import { TransitionPanel } from "@/registry/open-source/tabs-transition-panel";
+import { TabsTransitionPanel } from "@/registry/open-source/tabs-transition-panel";
 
 export default function Usage() {
 	return (
 		<div className="h-screen w-full flex items-center justify-center relative overflow-hidden bg-black">
-			<TransitionPanel />
+			<TabsTransitionPanel />
 		</div>
 	);
 }

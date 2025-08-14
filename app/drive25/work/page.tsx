e07b8dd4e381a938f4f2802463f19e48edd48ab7
dancrump1 <danielcrump@outlook.com>
2025-08-14 12:54:57 -0400

    update work page a little

--- app/drive25/work/page.tsx
+++ app/drive25/work/page.tsx
@@ -3,8 +3,121 @@
 import React from "react";
 
 import CursorFollow from "@/registry/open-source/cursor-follow";
+import { Fade, Slide } from "react-awesome-reveal";
 
 const images = [
+	{
+		src: "/itjustworks.jpg",
+		label: "Somethign about Cranmore",
+	},
+	{
+		src: "/itjustworks.jpg",
+		label: "Something about settlers green",
+	},
+	{
+		src: "/itjustworks.jpg",
+		label: "Something about State Parks",
+	},
+	{
+		src: "/itjustworks.jpg",
+		label: "Something about New Hampshire",
+	},
+	{
+		src: "/itjustworks.jpg",
+		label: "Somethign about Cranmore",
+	},
+	{
+		src: "/itjustworks.jpg",
+		label: "Something about settlers green",
+	},
+	{
+		src: "/itjustworks.jpg",
+		label: "Something about State Parks",
+	},
+	{
+		src: "/itjustworks.jpg",
+		label: "Something about New Hampshire",
+	},
+	{
+		src: "/itjustworks.jpg",
+		label: "Somethign about Cranmore",
+	},
+	{
+		src: "/itjustworks.jpg",
+		label: "Something about settlers green",
+	},
+	{
+		src: "/itjustworks.jpg",
+		label: "Something about State Parks",
+	},
+	{
+		src: "/itjustworks.jpg",
+		label: "Something about New Hampshire",
+	},
+	{
+		src: "/itjustworks.jpg",
+		label: "Somethign about Cranmore",
+	},
+	{
+		src: "/itjustworks.jpg",
+		label: "Something about settlers green",
+	},
+	{
+		src: "/itjustworks.jpg",
+		label: "Something about State Parks",
+	},
+	{
+		src: "/itjustworks.jpg",
+		label: "Something about New Hampshire",
+	},
+	{
+		src: "/itjustworks.jpg",
+		label: "Somethign about Cranmore",
+	},
+	{
+		src: "/itjustworks.jpg",
+		label: "Something about settlers green",
+	},
+	{
+		src: "/itjustworks.jpg",
+		label: "Something about State Parks",
+	},
+	{
+		src: "/itjustworks.jpg",
+		label: "Something about New Hampshire",
+	},
+	{
+		src: "/itjustworks.jpg",
+		label: "Somethign about Cranmore",
+	},
+	{
+		src: "/itjustworks.jpg",
+		label: "Something about settlers green",
+	},
+	{
+		src: "/itjustworks.jpg",
+		label: "Something about State Parks",
+	},
+	{
+		src: "/itjustworks.jpg",
+		label: "Something about New Hampshire",
+	},
+	{
+		src: "/itjustworks.jpg",
+		label: "Somethign about Cranmore",
+	},
+	{
+		src: "/itjustworks.jpg",
+		label: "Something about settlers green",
+	},
+	{
+		src: "/itjustworks.jpg",
+		label: "Something about State Parks",
+	},
+	{
+		src: "/itjustworks.jpg",
+		label: "Something about New Hampshire",
+	},
 	{
 		src: "/itjustworks.jpg",
 		label: "Somethign about Cranmore",
@@ -29,15 +142,27 @@ function Work() {
 			<CursorFollow>
 				<div className="flex flex-row items-center justify-center gap-8 py-8 flex-wrap">
 					{images.map((img, i) => (
-						<div key={i} className="flex flex-col items-center">
-							<img
-								src={img.src}
-								alt={img.label}
-								data-cursor-text={img.label}
-								className="border-background h-48 w-48 rounded-xl object-cover transition-transform duration-200 hover:scale-105"
-								// style={{ cursor: "none" }}
-							/>
-						</div>
+						<Slide
+							direction="up"
+							delay={i % 2 === 0 ? 500 : 0}
+							triggerOnce
+						>
+							<Fade
+								fraction={0.5}
+								duration={1000} // Animation duration
+								triggerOnce // Animate only once
+							>
+								<div key={i} className="flex flex-col items-center">
+									<img
+										src={img.src}
+										alt={img.label}
+										data-cursor-text={img.label}
+										className="border-background h-48 w-[33vw] rounded-xl object-cover transition-transform duration-200 hover:scale-105"
+										// style={{ cursor: "none" }}
+									/>
+								</div>
+							</Fade>
+						</Slide>
 					))}
 				</div>
 			</CursorFollow>

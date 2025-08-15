0ca7d59dd64f3e9ed11ea6d6097c1d7ce868e2ca
dancrump1 <danielcrump@outlook.com>
2025-08-15 15:13:34 -0400

    last file needed

--- app/drive25/api/preview/route.ts
+++ app/drive25/api/preview/route.ts
@@ -1,5 +1,5 @@
-import { draftMode } from "next/headers";
-import { NextResponse } from "next/server";
+import { cookies, draftMode } from "next/headers";
+import { redirect } from "next/navigation";
 
 import cmsClient from "@/lib/cmsClient";
 import { gql } from "graphql-request";
@@ -7,7 +7,6 @@ import { gql } from "graphql-request";
 // This matches GET /api/preview
 export async function GET(request: Request) {
 	const { searchParams } = new URL(request.url);
-	const token = searchParams.get("token");
 	const entryUid = searchParams.get("entryUid");
 
 	// Optionally validate token
@@ -15,7 +14,13 @@ export async function GET(request: Request) {
 	//   return NextResponse.json({ message: 'Token missing' }, { status: 401 })
 	// }
 
-	const client = cmsClient(token || null);
+	// Check the secret and next parameters
+	// This secret should only be known to this Route Handler and the CMS
+	//   if (secret !== 'secretsecret123') {
+	//     return new Response('Invalid token', { status: 401 })
+	//   }
+
+	const client = cmsClient(false, "");
 
 	const pageQuery = gql`
 		query ($uid: [String]) {
@@ -29,26 +34,15 @@ export async function GET(request: Request) {
 
 	const data = await client.request(pageQuery, { uid: entryUid });
 
-	if (!data?.entry?.uri) {
-		return NextResponse.json(
-			{ message: `URL of the entry ${entryUid} could not be fetched` },
-			{ status: 404 }
-		);
-	}
-
-	const { uri, typeHandle } = data.entry;
+	const { uri } = data.entry;
 	const isHome = uri === "__home__" || uri === "home";
 	const location = `/${isHome ? "drive25" : "drive25/" + uri}`;
 
 	// In App Router, setPreviewData is still available but only in *pages router*
 	// Instead, use draftMode()
-	(await draftMode()).enable();
+	const draft = await draftMode();
+	draft.enable();
 
 	// Store extra data in cookies if needed
-	const response = NextResponse.redirect(new URL(location, request.url));
-	response.cookies.set("entryUid", entryUid || "", { path: "/" });
-	response.cookies.set("typeHandle", typeHandle || "", { path: "/" });
-	response.cookies.set("token", token || "", { path: "/" });
-
-	return response;
+	redirect(location);
 }

b33f8de2c8a06d25e08973a3fa5dcf460825d862
dancrump1 <danielcrump@outlook.com>
2025-08-21 14:54:21 -0400

    add cat accordion

---
+++ components/usages/sliceaccordionusage.tsx
@@ -0,0 +1,9 @@
+import SliceAccordion from "@/registry/open-source/slice-accordion";
+
+export default function Usage() {
+	return (
+		<div className="h-screen w-full flex items-center justify-center relative overflow-hidden bg-background">
+			<SliceAccordion />
+		</div>
+	);
+}

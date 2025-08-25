051ea66b99c60050de2ff710187d7d09deb79053
dancrump1 <danielcrump@outlook.com>
2025-08-25 10:25:38 -0400

    fix import

--- registry/open-source/icons/construction.tsx
+++ registry/open-source/icons/construction.tsx
@@ -3,7 +3,7 @@
 import type { HTMLAttributes } from "react";
 import { forwardRef, useCallback, useImperativeHandle, useRef } from "react";
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/registry/utilities/cn";
 import { motion, useAnimation } from "motion/react";
 
 export interface ConstructionIconHandle {

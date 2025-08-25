2f05f4bb1ce05c4ab52cb734869ece27e4e52c1a
dancrump1 <danielcrump@outlook.com>
2025-08-25 14:03:00 -0400

    woops

--- app/drive25/layout.tsx
+++ app/drive25/layout.tsx
@@ -8,6 +8,7 @@ import "./transition.css";
 import ColorSelector from "@/components/ColorSelector";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import { HoverProvider } from "@/lib/hover-context";
+import HoverFooter from "@/registry/open-source/hover-footer";
 import ResizeNavBar from "@/registry/open-source/resize-navbar";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
@@ -52,6 +53,7 @@ export default async function RootLayout({
 					</NuqsAdapter>
 				</ThemeProvider>
 				{/* </ColorSelector> */}
+				<HoverFooter />
 			</body>
 			<GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_KEY} />
 		</html>

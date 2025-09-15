8c24866c0eefa9c2719e3a359b45c0e891b900af
dancrump1 <danielcrump@outlook.com>
2025-09-15 15:31:10 -0400

    add testimonial grid

---
+++ components/usages/testimonialgridusage.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+import { TestimonialsGrid } from "@/registry/open-source/testimonial-grid";
+
+export default function Usage() {
+	return (
+		<div className="h-screen w-full flex items-center justify-center relative overflow-hidden bg-background">
+			<TestimonialsGrid />
+		</div>
+	);
+}
